44e78d3e2660bb89a5eb322f2e9e2d01966b17b6
Muhammad Danial Siddiqui <muhammaddanialsiddiqui@gmail.com>
2022-05-16 20:55:29 +0500

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,3 +1,5 @@
-area = int(input("Enter the Number : "))
-convert  = area * 42560
-print("You have this Acre in area : ", convert)
+# area = int(input("Enter the Number : "))
+# convert  = area * 42560
+# print("You have this Acre in area : ", convert)
+
+print("Hello World!")
